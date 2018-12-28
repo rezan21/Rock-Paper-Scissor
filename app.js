c7e111aef78aa26396107a9d9720bb23168f6d02
rezan21 <reza.naghshineh@gmail.com>
2018-12-28 15:10:40 +0000

    css and html done

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('hello');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissor_div = document.getElementById('s');
+const userScore = document.getElementById('user-score');
